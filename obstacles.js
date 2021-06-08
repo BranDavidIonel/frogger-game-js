fd6c3cf136319871e42d6a8a75a7a7f831d88f06
david <bran.david.ionel@gmail.com>
2021-06-08 19:17:54 +0300

    reset cars after the edge

--- obstacles.js
+++ obstacles.js
@@ -8,10 +8,14 @@ class Obstacles{
 		this.type=type;
 	}
 	draw(){
-		ctx1.fillRect(this.x,this.y,this.watch,this.height);
+		ctx1.fillStyle='pink';
+		ctx1.fillRect(this.x,this.y,this.width,this.height);
 	}
 	update(){
 		this.x+=this.speed*gameSpeed;
+		if(this.x>canvas.width+this.width){
+			this.x=0-this.width;
+		}
 		
 	}	
 }
@@ -19,7 +23,13 @@ function initObstacles(){
 	//line 1
 	for(let i=0;i<2;i++){
 		let x=i*350;
-		carsArray.push(new Obstacles(x,canvas.height-grid*2-20,grid,grid,1,'car'));
+		carsArray.push(new Obstacles(x,canvas.height-grid*2-20,grid*2,grid,1,'car'));
 	}
 }
 initObstacles();
+function handleObstacles(){
+	for(let i=0;i<carsArray.length;i++){
+		carsArray[i].update();
+		carsArray[i].draw();
+	}
+}
