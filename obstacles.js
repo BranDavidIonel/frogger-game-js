2c613c2d82a1a6dcad662513ed642bf4cb5d4fb9
david <bran.david.ionel@gmail.com>
2021-06-08 20:25:46 +0300

    multi bands(with different sens)

--- obstacles.js
+++ obstacles.js
@@ -1,34 +1,50 @@
-class Obstacles{
-	constructor(x,y,width,height,speed,type){
-		this.x=x;
-		this.y=y;
-		this.width=width;
-		this.height=height;
-		this.speed=speed;
-		this.type=type;
+class Obstacles {
+	constructor(x, y, width, height, speed, type) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		this.speed = speed;
+		this.type = type;
 	}
-	draw(){
-		ctx1.fillStyle='pink';
-		ctx1.fillRect(this.x,this.y,this.width,this.height);
+	draw() {
+		ctx1.fillStyle = 'pink';
+		ctx1.fillRect(this.x, this.y, this.width, this.height);
 	}
-	update(){
-		this.x+=this.speed*gameSpeed;
-		if(this.x>canvas.width+this.width){
-			this.x=0-this.width;
+	update() {
+		this.x += this.speed * gameSpeed;
+		if (this.speed > 0) {
+			if (this.x > canvas.width + this.width) {
+				this.x = 0 - this.width;
+			}
+		}else{
+			if(this.x<0-this.width){
+				this.x=canvas.width+this.width;
+			}
 		}
-		
-	}	
+
+	}
 }
-function initObstacles(){
-	//line 1
-	for(let i=0;i<2;i++){
-		let x=i*350;
-		carsArray.push(new Obstacles(x,canvas.height-grid*2-20,grid*2,grid,1,'car'));
+function initObstacles() {
+	//lane 1
+	for (let i = 0; i < 2; i++) {
+		let x = i * 350;
+		carsArray.push(new Obstacles(x, canvas.height - grid * 2 - 20, grid * 2, grid, 1, 'car'));
+	}
+	//lane 2
+	for (let i = 0; i < 2; i++) {
+		let x = i * 300;
+		carsArray.push(new Obstacles(x, canvas.height - grid * 3 - 20, grid * 2, grid, -10, 'car'));
+	}
+	//lane 3
+	for (let i = 0; i < 2; i++) {
+		let x = i * 400;
+		carsArray.push(new Obstacles(x, canvas.height - grid * 4 - 20, grid, grid, 4, 'car'));
 	}
 }
 initObstacles();
-function handleObstacles(){
-	for(let i=0;i<carsArray.length;i++){
+function handleObstacles() {
+	for (let i = 0; i < carsArray.length; i++) {
 		carsArray[i].update();
 		carsArray[i].draw();
 	}
