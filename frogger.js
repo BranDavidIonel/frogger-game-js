8cf3898a1b25b8eed10d95d56c04be942abda2a5
david <bran.david.ionel@gmail.com>
2021-06-09 20:12:13 +0300

    bug turtle

--- frogger.js
+++ frogger.js
@@ -1,72 +1,72 @@
-class Frogger{
-	constructor(){
-		this.spriteWidth=250;
-		this.spriteHeight=250;
-		this.width=this.spriteWidth/5;
-		this.height=this.spriteHeight/5;
-		this.x=canvas.width/2-this.width/2;
-		this.y=canvas.height-this.height-40;
-		this.moving=false;
-		this.frameX=0;
-		this.frameY=0;
-		
+class Frogger {
+	constructor() {
+		this.spriteWidth = 250;
+		this.spriteHeight = 250;
+		this.width = this.spriteWidth / 5;
+		this.height = this.spriteHeight / 5;
+		this.x = canvas.width / 2 - this.width / 2;
+		this.y = canvas.height - this.height - 40;
+		this.moving = false;
+		this.frameX = 0;
+		this.frameY = 0;
+
 	}
-	update(){
+	update() {
 		//console.log('update');
-		if(keys[38]){///up
-			if(this.moving === false){
-				this.y-=grid;
-				this.moving=true;
-				this.frameX=1;
-				this.frameY=0;
+		if (keys[38]) {///up
+			if (this.moving === false) {
+				this.y -= grid;
+				this.moving = true;
+				this.frameX = 1;
+				this.frameY = 0;
 
 			}
-			
+
 		}
-		if(keys[40]){///DOWN
-			if(this.y<canvas.height-this.height*2 && this.moving === false){
-				this.y+=grid;
-				this.moving=true;
-				this.frameX=1;
-				this.frameY=3;
+		if (keys[40]) {///DOWN
+			if (this.y < canvas.height - this.height * 2 && this.moving === false) {
+				this.y += grid;
+				this.moving = true;
+				this.frameX = 1;
+				this.frameY = 3;
 			}
-			
+
 		}
-		if(keys[37]){///left
-			if(this.x>this.width && this.moving === false){
-				this.x-=grid;
-				this.moving=true;
-				this.frameX=1;
-				this.frameY=2;
+		if (keys[37]) {///left
+			if (this.x > this.width && this.moving === false) {
+				this.x -= grid;
+				this.moving = true;
+				this.frameX = 1;
+				this.frameY = 2;
 			}
-			
+
 		}
-		if(keys[39]){///right
-			if(this.x<(canvas.width-this.width*2) && this.moving === false){
-				this.x+=grid;
-				this.moving=true;
-				this.frameX=1;
-				this.frameY=1;
+		if (keys[39]) {///right
+			if (this.x < (canvas.width - this.width * 2) && this.moving === false) {
+				this.x += grid;
+				this.moving = true;
+				this.frameX = 1;
+				this.frameY = 1;
 			}
-			
+
 		}
-		if(this.y<0)scored();
-		
-	 
+		if (this.y < 0) scored();
+
+
 	}
-	draw(){
+	draw() {
 		//ctx3.fillStyle='green';
 		//ctx3.fillRect(this.x,this.y,this.width,this.height);
-		ctx3.drawImage(froggerSprite,this.frameX*this.spriteWidth,this.frameY*this.spriteHeight,this.spriteWidth,this.spriteHeight,this.x-25,this.y-25,this.width*2,this.height*2)
-		
+		ctx3.drawImage(froggerSprite, this.frameX * this.spriteWidth, this.frameY * this.spriteHeight, this.spriteWidth, this.spriteHeight, this.x - 25, this.y - 25, this.width * 2, this.height * 2)
+
 	}
-	jump(){
-		if(this.moving===false){
-			this.frameX=1
-		}else if(this.frameX==1){
-			this.frameX=0;
+	jump() {
+		if (this.moving === false) {
+			this.frameX = 1
+		} else if (this.frameX == 1) {
+			this.frameX = 0;
 		}
 	}
-	
+
 }
-const frogger=new Frogger();
+const frogger = new Frogger();
--- utilities.js
+++ utilities.js
@@ -1,74 +1,74 @@
-function animate(){
-	ctx1.clearRect(0,0,canvas.width,canvas.height);
-	ctx2.clearRect(0,0,canvas.width,canvas.height);
-	ctx3.clearRect(0,0,canvas.width,canvas.height);
-	ctx3.clearRect(0,0,canvas.width,canvas.height);
-	ctx4.clearRect(0,0,canvas.width,canvas.height);
-	ctx5.clearRect(0,0,canvas.width,canvas.height);
+function animate() {
+	ctx1.clearRect(0, 0, canvas.width, canvas.height);
+	ctx2.clearRect(0, 0, canvas.width, canvas.height);
+	ctx3.clearRect(0, 0, canvas.width, canvas.height);
+	ctx3.clearRect(0, 0, canvas.width, canvas.height);
+	ctx4.clearRect(0, 0, canvas.width, canvas.height);
+	ctx5.clearRect(0, 0, canvas.width, canvas.height);
 	handleRipples();
-	ctx2.drawImage(background_lvl2,0,0,canvas.width,canvas.height);
+	ctx2.drawImage(background_lvl2, 0, 0);
 	handleParticles();
-	
+
 	frogger.draw();
 	frogger.update();
-	
+
 	handleObstacles();
-	ctx4.drawImage(grass,0,0,canvas.width,canvas.height);
 	handleScoreBoard();
+	ctx4.drawImage(grass, 0, 0, canvas.width, canvas.height);
 	frame++;
 	requestAnimationFrame(animate);
-	
-	
+
+
 }
 animate();
 
 //event listeners
 
-window.addEventListener('keydown',function(e){
-	key=[];
-	keys[e.keyCode]=true;
-	if(keys[37]||key[38]||key[39]||key[40]){
+window.addEventListener('keydown', function (e) {
+	key = [];
+	keys[e.keyCode] = true;
+	if (keys[37] || key[38] || key[39] || key[40]) {
 		frogger.jump();
 	}
-	
+
 });
-window.addEventListener('keyup',function(e){
+window.addEventListener('keyup', function (e) {
 	delete keys[e.keyCode];
-	frogger.moving=false;
-	frogger.frameX=0;
+	frogger.moving = false;
+	frogger.frameX = 0;
 });
-function scored(){
+function scored() {
 	score++;
-	gameSpeed+=0.05;
-	frogger.x=canvas.width/2-frogger.width/2;
-	frogger.y=canvas.height-frogger.height-40;
-	
+	gameSpeed += 0.05;
+	frogger.x = canvas.width / 2 - frogger.width / 2;
+	frogger.y = canvas.height - frogger.height - 40;
+
 }
-function handleScoreBoard(){
-	ctx4.fillStyle='black';
-	ctx4.strokeStyle='black';
-	ctx4.font='15px Verdana';
-	ctx4.strokeText('Score',265,15);
-	ctx4.font='60px Verdana';
-	ctx4.fillText(score,270,65);
-	ctx4.font='15px Verdana';
-	ctx4.strokeText('Collisions:'+collisionsCount,10,175);
-	ctx4.strokeText('Game Speed:'+gameSpeed.toFixed(1),10,195);
+function handleScoreBoard() {
+	ctx4.fillStyle = 'black';
+	ctx4.strokeStyle = 'black';
+	ctx4.font = '15px Verdana';
+	ctx4.strokeText('Score', 265, 15);
+	ctx4.font = '60px Verdana';
+	ctx4.fillText(score, 270, 65);
+	ctx4.font = '15px Verdana';
+	ctx4.strokeText('Collisions:' + collisionsCount, 10, 175);
+	ctx4.strokeText('Game Speed:' + gameSpeed.toFixed(1), 10, 195);
 
 }
 //collesion detection between thw rectangles
-function collision(first,second){
+function collision(first, second) {
 	//rectangles are not overlapping condition
-	return !(first.x>second.x+second.width ||
-			first.x+first.width<second.x ||
-			first.y>second.y+second.height||
-			first.y+first.height<second.y);
+	return !(first.x > second.x + second.width ||
+		first.x + first.width < second.x ||
+		first.y > second.y + second.height ||
+		first.y + first.height < second.y);
 }
 
-function resetGame(){
-	score=0;
-	gameSpeed=1;
-	frogger.x=canvas.width/2-frogger.width/2;
-	frogger.y=canvas.height-frogger.height-40;
+function resetGame() {
+	score = 0;
+	gameSpeed = 1;
+	frogger.x = canvas.width / 2 - frogger.width / 2;
+	frogger.y = canvas.height - frogger.height - 40;
 
 }
